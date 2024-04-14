5b4d9f846eb11f9d40ccedc4beb4e51de04a489f
jiohLee <jioh0826@outlook.kr>
2024-04-14 11:41:29 +0000

    write download script

--- download_results.py
+++ download_results.py
@@ -1,11 +1,44 @@
+from itertools import product
 import time, datetime
 import wandb
 
 pjt_name = "jioh0826/cs-spec"
 
+table_dataset = [
+    "synthetic",
+    "synthetic-n40db",
+    "synthetic-n30db",
+    "synthetic-n20db",
+    "synthetic-n35db",
+    "synthetic-n25db",
+    "synthetic-n15db",
+    "measured",
+    "drink-pink",
+    "drink-gold",
+    "drink-pyellow",
+    "drink-blue",
+    "drink-purple",
+]
+
+table_model = [
+    "cnn-wa",
+    "cnn-wagu",
+    "cnn-woa",
+    "rescnn-wa",
+    "rescnn-wagu",
+    "rescnn-woa",
+    "resunet-wa",
+    "resunet-wagu",
+    "resunet-woa",
+]
+
+num_exp = "01"
+
 api = wandb.Api()
 start = time.time()
-for run in api.runs("jioh0826/cs-spec"):
+table_run = {run.name: run for run in api.runs(pjt_name)}
+for dataset_name, model_name in product(table_dataset, table_model):
+    run = table_run[f"{model_name}-{dataset_name}-{num_exp}"]
     run.file(f"results/{run.name}/y.csv").download(exist_ok=True)
     run.file(f"results/{run.name}/x.csv").download(exist_ok=True)
     run.file(f"results/{run.name}/xr.csv").download(exist_ok=True)
